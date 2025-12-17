3f8a3c6287b9179b92e69e73a71b71358162a6d4
v1perii2572 <v1perii2570202@gmail.com>
2025-12-17 09:18:32 +0700

    Update StaffServlet.java

--- src/java/controllers/StaffServlet.java
+++ src/java/controllers/StaffServlet.java
@@ -9,7 +9,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
 import models.Role;
 import models.Staff;
